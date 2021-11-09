7c13fd1919547272b20c02e84467dac0d9a95b43
suryapampana <17pa5a0320@vishnu.edu.in>
2021-11-10 03:00:23 +0530

    Done Sign & Registration

---
+++ src/app/registrationService/registration.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Employee } from '../employee/employee';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  
+
+  constructor(private _http : HttpClient ) { }
+
+  public loginEmployeeFromRemote(employee: Employee):Observable<any>{
+    return this._http.post<any>("http://localhost:8080/loginemployee",employee)
+
+  }
+
+  public registerEmployeeFromRemote(employee :Employee):Observable<any>{
+    return this._http.post<any>("http://localhost:8080/registeremployee", employee);
+  }
+
+  handleError(error: Response){
+    this.message = error;
+          this.status = false;
+
+  }
+}
